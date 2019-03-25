4d82ea4e6db82c98cf343b961fbb92788eb5e04c
paul <pdmxdd@gmail.com>
2019-03-25 00:19:18 -0500

    Try/Except for GET /status

--- main.py
+++ main.py
@@ -21,22 +21,22 @@ def index():
 
 @app.route('/status', methods=['GET'])
 def get_status():
-    with open('status.json', 'r') as rf:
-        read_json = json.load(rf)
-        print(read_json)
-        return make_json_response(json.dumps(read_json), 200)
-    return 'what'
+    try:
+        with open('status.json', 'r') as rf:
+            read_json = json.load(rf)
+            return make_json_response(json.dumps(read_json), 200)
+    except FileNotFoundError:
+        return make_json_response(json.dumps({"error": "No JSON on server"}), 400)
 
 
 @app.route('/status', methods=['POST'])
 def post_status():
-    print('hello')
     if request.json is not None:
-        print(request.json)
         with open('status.json', 'w') as wf:
             json.dump(request.json, wf)
         return make_json_response(json.dumps(request.json), 201)
     return make_json_response(json.dumps({"error": "json not included with request"}), 400)
+
 
 if __name__ == '__main__':
     app.run(port=8080)
