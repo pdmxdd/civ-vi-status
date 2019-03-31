2e5e76dc4c95ab8457619954cbf4c9132f4d188d
paul <pdmxdd@gmail.com>
2019-03-31 14:41:20 -0500

    Adds time

--- app/main.py
+++ app/main.py
@@ -1,8 +1,46 @@
 from flask import Flask, make_response, request, render_template
 import json
+import datetime
 
 app = Flask(__name__)
 
+
+def central_time(datetime_string):
+    datetime_string_split = datetime_string.split("/")
+    central_year = int(datetime_string_split[0])
+    central_month = int(datetime_string_split[1])
+    central_day = int(datetime_string_split[2])
+    central_hour = int(datetime_string_split[3][0:2]) - 5
+    if central_hour < 0:
+        central_hour += 24
+        central_day = int(datetime_string_split[2]) - 1
+        if central_day == 0:
+            if datetime_string_split[1] in ["01", "03", "05", "07", "08", "10", "12"]:
+                central_day = 31
+            elif datetime_string_split[1] in ["04", "06", "09", "11"]:
+                central_day = 30
+            else:
+                # TODO: if leapyear(): central_day = 29 if leapyear(): else central_day = 28
+                central_day = 28
+            central_month = int(datetime_string_split[1]) - 1
+            if central_month == 0:
+                central_month = 12
+                central_year = int(datetime_string_split[0]) - 1
+
+    if central_day < 10:
+        central_day = "0{}".format(central_day)
+    if central_month < 10:
+        central_month = "0{}".format(central_month)
+    if central_hour < 10:
+        central_hour = "0{}".format(central_hour)
+
+    str_civ_time = "{}/{}/{}/{}{}".format(central_year,
+                                          central_month,
+                                          central_day,
+                                          central_hour,
+                                          datetime_string_split[3][2:])
+
+    return str_civ_time
 
 def make_json_response(the_json, status_code):
     """
@@ -24,6 +62,7 @@ def get_status():
     try:
         with open('status.json', 'r') as rf:
             read_json = json.load(rf)
+            read_json["current_time"] = central_time(datetime.datetime.utcnow().strftime("%Y/%m/%d/%H:%M:%S"))
             return make_json_response(json.dumps(read_json), 200)
     except FileNotFoundError:
         return make_json_response(json.dumps({"error": "No JSON on server"}), 400)
@@ -33,6 +72,7 @@ def get_status():
 def post_status():
     if request.json is not None:
         game_json = {}
+        game_json["turn_time"] = central_time(datetime.datetime.utcnow().strftime("%Y/%m/%d/%H:%M:%S"))
         if "value1" in request.json.keys():
             game_json["name"] = request.json["value1"]
         if "value2" in request.json.keys():
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,15 +4,18 @@ function gameRefresh() {
         let gameName = json["name"];
         let player = json["player"];
         let turn = json["turn"];
+        let turnTime = json["turn_time"];
+        let currentTime = json["current_time"];
         $('#gameInfo').empty();
         $('#gameInfo').append(`
                                <h1>It is ${player}'s turn</h1>
                                <p>in ${gameName} turn: #${turn}</p>
+                               <p>turn taken at ${turnTime}</p>
+                               <p>current time: ${currentTime}</p>
                               `);
     });
 };
 
 $(document).ready(function () {
-    //console.log("scripts.js loaded");
     gameRefresh();
 });
